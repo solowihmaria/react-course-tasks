1874aebfccc863fd565c9db323db7b973a5377f5
solowihmaria <solowihmaria@gmail.com>
2025-07-12 19:24:22 +0300

    feat: setup app entry point and wrap in ErrorBoundary

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary';
 
 const rootElement = document.getElementById('root');
 
 if (rootElement) {
   createRoot(rootElement).render(
     <StrictMode>
-      <App />
+      <ErrorBoundary>
+        <App />
+      </ErrorBoundary>
     </StrictMode>
   );
 }
