11ca696b3da8c509e2954ca36c03083c0350d1ca
solowihmaria <solowihmaria@gmail.com>
2025-07-24 19:44:47 +0300

    feat: add Footer component

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import styles from './Footer.module.css';
+
+export const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.content}>
+        <p>© 2025 Pokémon App - RS School</p>
+      </div>
+    </footer>
+  );
+};
