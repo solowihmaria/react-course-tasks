f5ba9c71fa779ff435794941c8952a0c6c398256
solowihmaria <solowihmaria@gmail.com>
2025-07-25 20:25:42 +0300

    refactor: convert Card class component to functional

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,4 +1,3 @@
-import { Component } from 'react';
 import styles from './Card.module.css';
 import type { Pokemon } from '../../types/types';
 
@@ -7,44 +6,40 @@ interface CardProps {
   compact?: boolean;
 }
 
-class Card extends Component<CardProps> {
-  render() {
-    const { item, compact = false } = this.props;
-    // вариант,когда список всех
-    if (compact) {
-      return (
-        <div className={styles['card-compact']}>
-          <img
-            src={item.sprites?.front_default || '/placeholder-pokemon.png'}
-            alt={item.name}
-            className={styles['pokemon-thumbnail']}
-          />
-          <div className={styles['pokemon-info']}>
-            <h4>{item.name}</h4>
-            <p className={styles.types}>
-              {item.types?.map((t) => t.type.name).join(', ')}
-            </p>
-          </div>
-        </div>
-      );
-    }
-    // вариант,когда ищем конкретного
+export const Card = ({ item, compact = false }: CardProps) => {
+  // вариант,когда список всех
+  if (compact) {
     return (
-      <div className={styles['card-detailed']}>
+      <div className={styles['card-compact']}>
         <img
           src={item.sprites?.front_default || '/placeholder-pokemon.png'}
           alt={item.name}
-          className={styles['pokemon-image']}
+          className={styles['pokemon-thumbnail']}
         />
-        <div className={styles['pokemon-details']}>
-          <h3>{item.name.toUpperCase()}</h3>
-          <p>Type: {item.types?.map((t) => t.type.name).join(', ')}</p>
-          <p>Height: {(item.height || 0) / 10}m</p>
-          <p>Weight: {(item.weight || 0) / 10}kg</p>
+        <div className={styles['pokemon-info']}>
+          <h4>{item.name}</h4>
+          <p className={styles.types}>
+            {item.types?.map((t) => t.type.name).join(', ')}
+          </p>
         </div>
       </div>
     );
   }
-}
 
-export default Card;
+  // вариант,когда ищем конкретного
+  return (
+    <div className={styles['card-detailed']}>
+      <img
+        src={item.sprites?.front_default || '/placeholder-pokemon.png'}
+        alt={item.name}
+        className={styles['pokemon-image']}
+      />
+      <div className={styles['pokemon-details']}>
+        <h3>{item.name.toUpperCase()}</h3>
+        <p>Type: {item.types?.map((t) => t.type.name).join(', ')}</p>
+        <p>Height: {(item.height || 0) / 10}m</p>
+        <p>Weight: {(item.weight || 0) / 10}kg</p>
+      </div>
+    </div>
+  );
+};
