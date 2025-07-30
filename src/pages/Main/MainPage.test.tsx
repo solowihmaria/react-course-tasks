666120eb61e69291453b2f6934faea7781b82b0f
solowihmaria <solowihmaria@gmail.com>
2025-07-30 19:01:01 +0300

    refactor: update tests

--- src/pages/Main/MainPage.test.tsx
+++ src/pages/Main/MainPage.test.tsx
@@ -2,15 +2,19 @@ import { describe, it, expect } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import { MemoryRouter, Route, Routes } from 'react-router-dom';
 import { MainPage } from './MainPage';
+import { RightSide } from './parts/RightSide/RightSide';
+import { ThemeProvider } from '../../contexts/ThemeProvider';
 
 describe('MainPage', () => {
   it('renders LeftSide by default without details', () => {
     render(
-      <MemoryRouter initialEntries={['/1']}>
-        <Routes>
-          <Route path="/:page" element={<MainPage />} />
-        </Routes>
-      </MemoryRouter>
+      <ThemeProvider>
+        <MemoryRouter initialEntries={['/1']}>
+          <Routes>
+            <Route path="/:page" element={<MainPage />} />
+          </Routes>
+        </MemoryRouter>
+      </ThemeProvider>
     );
 
     expect(screen.getByPlaceholderText(/search/i)).toBeInTheDocument();
@@ -18,11 +22,15 @@ describe('MainPage', () => {
 
   it('renders RightSide when pokemonId is present', () => {
     render(
-      <MemoryRouter initialEntries={['/1/25']}>
-        <Routes>
-          <Route path="/:page/:pokemonId" element={<MainPage />} />
-        </Routes>
-      </MemoryRouter>
+      <ThemeProvider>
+        <MemoryRouter initialEntries={['/1/25']}>
+          <Routes>
+            <Route path="/:page" element={<MainPage />}>
+              <Route path=":pokemonId" element={<RightSide />} />
+            </Route>
+          </Routes>
+        </MemoryRouter>
+      </ThemeProvider>
     );
 
     expect(
