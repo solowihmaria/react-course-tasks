997a469c7139920a2e475cf4416fd08b9afb8234
solowihmaria <solowihmaria@gmail.com>
2025-08-02 17:08:29 +0300

    feat: integrate Redux selection logic

--- src/components/CardList/CardList.tsx
+++ src/components/CardList/CardList.tsx
@@ -1,6 +1,9 @@
 import { useNavigate, useParams } from 'react-router-dom';
 import { Card } from '../Card/Card';
 import { Loader } from '../Loader/Loader';
+import { SelectionFlyout } from '../SelectionFlyout/SelectionFlyout';
+import { useAppSelector, useAppDispatch } from '../../store/hooks';
+import { togglePokemon } from '../../store/slices/selectionSlice';
 import type { Pokemon } from '../../types/types';
 import styles from './CardList.module.css';
 
@@ -14,6 +17,14 @@ interface CardListProps {
 export const CardList = ({ items, isLoading, error }: CardListProps) => {
   const navigate = useNavigate();
   const { page } = useParams();
+
+  const dispatch = useAppDispatch();
+  const selectedIds = useAppSelector((state) => state.selection.selectedIds);
+
+  const handleToggleSelect = (id: number) => {
+    dispatch(togglePokemon(id));
+  };
+
   if (error) {
     return <div className={styles['error-message']}>{error}</div>;
   }
@@ -27,15 +38,20 @@ export const CardList = ({ items, isLoading, error }: CardListProps) => {
   }
 
   return (
-    <div className={styles['card-list']}>
-      {items.map((item) => (
-        <Card
-          key={item.id}
-          item={item}
-          compact={true}
-          onClick={() => navigate(`/${page}/${item.id}`)}
-        />
-      ))}
+    <div className={styles.container}>
+      <div className={styles['card-list']}>
+        {items.map((item) => (
+          <Card
+            key={item.id}
+            item={item}
+            compact={true}
+            isSelected={!!selectedIds[item.id ?? 0]}
+            onToggleSelect={handleToggleSelect}
+            onClick={() => navigate(`/${page}/${item.id}`)}
+          />
+        ))}
+      </div>
+      <SelectionFlyout />
     </div>
   );
 };
