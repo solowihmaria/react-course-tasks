8c92f4483602f1556201d95e48d2b5522e6059cc
solowihmaria <solowihmaria@gmail.com>
2025-08-02 17:02:27 +0300

    feat: add store Provider

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,19 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import './assets/styles/main.css';
 import { ThemeProvider } from './contexts/ThemeProvider';
+import { Provider } from 'react-redux';
+import { store } from './store/store';
 
 const rootElement = document.getElementById('root');
 
 if (rootElement) {
   createRoot(rootElement).render(
-    <ThemeProvider>
-      <StrictMode>
-        <App />
-      </StrictMode>
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider>
+        <StrictMode>
+          <App />
+        </StrictMode>
+      </ThemeProvider>
+    </Provider>
   );
 }
