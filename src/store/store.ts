12b4d51cd0335f19be7cacaaf5a59e43f0da3869
solowihmaria <solowihmaria@gmail.com>
2025-08-09 13:16:54 +0300

    feat: configure store with RTK Query middleware

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,10 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import selectionReducer from './slices/selectionSlice';
+import { pokemonApi } from './slices/pokemonApi';
 
 export const store = configureStore({
   reducer: {
     selection: selectionReducer,
+    [pokemonApi.reducerPath]: pokemonApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(pokemonApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
